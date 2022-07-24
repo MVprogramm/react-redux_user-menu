b3068ee1e5ee21589111dc10b668852e03f14cc4
Michael Valiahmetov <michael.valiahmetov@gmail.com>
2022-07-24 04:10:11 +0300

    add code

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from "./Header.jsx";
+import { UserDataContext } from "./context.js";
+import "./index.scss";
+
+class App extends React.Component {
+  state = {
+    userData: {
+      name: "Nicola Tesla",
+      avatar_url: "https://avatars3.githubusercontent.com/u10001",
+    }
+  }
+
+  render() {
+    return (
+      <UserDataContext.Provider value={this.state.userData} >
+        <div className="page">
+          <Header />
+        </div>
+      </UserDataContext.Provider>
+    );
+  }
+};
+
+export default App;
+
---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import UserMenu from "./UserMenu.jsx";
+
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="header">
+        <UserMenu />
+      </header>
+    )
+  }
+};
+  
+export default Header;
